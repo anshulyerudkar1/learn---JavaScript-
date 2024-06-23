915736921e66bfff2f096ef399a504c0bac092cd
Anshul Subhash Yerudkar <113240610+anshulyerudkar1@users.noreply.github.com>
2024-06-23 19:47:50 +0000

    operatins in javascript

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -26,3 +26,43 @@ let someNumber = 33
 let stringNumber = String(someNumber);
 console.log(stringNumber);
 
+// ********************************** Operations *************************************
+
+
+let value = 3
+let negValue = -value;
+//console.log(negValue);
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**3);
+//console.log(2/3);
+//console.log(2%3);
+
+
+let str1 = "Hello"
+let str2 = " Anshul"
+let str3 = str1 + str2;
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2 );
+console.log(1 + 2 + "2" );
+console.log(3 + 4 * 5 % 3 );
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3;
+num1 = num2 = num3 = 2+2;
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+gameCounter++;
+console.log(gameCounter);
+
+//link to study
+// https://tc39.es/exma262/multipage/abstract-operations.html#sec-type-conversion
