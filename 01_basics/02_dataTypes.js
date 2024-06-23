7d8a128601040f1295e36f3859d55cf76d1ef135
Anshul Subhash Yerudkar <113240610+anshulyerudkar1@users.noreply.github.com>
2024-06-23 19:04:30 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,32 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3+3)// we are using nodejs, not browser
+
+console.log(3
+    +3); // code readability should be high
+
+console.log("Ansh"); 
+
+
+let name = "Anshu"; // string
+let age = 27; // number
+let isLoggedIn = false; //boolean
+
+
+// number => 2 to power 53
+// bigint 
+// String => "" 
+// boolean => true/false
+// null => stand-alone value 
+// undefined => values not assigned 
+// symbol => unique
+
+
+// object
+console.log(typeof undefined); // undefined 
+console.log(typeof null); // object
+
+
+
+
+
